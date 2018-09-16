d4811ddfae03f390fc631420ba88b094f93c209f
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2018-09-16 14:09:18 +0100

    Update numberFunctions.js

--- numberFunctions.js
+++ numberFunctions.js
@@ -92,7 +92,7 @@ function firstFactorial(num) {
     var factorial = 1
 
 	for(var i = 1; i <= num; i++){
-        factorial = factorial * i;
+        factorial += factorial * i;
     }
 
 return factorial;
